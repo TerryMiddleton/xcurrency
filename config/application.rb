1b66dc53b3dc66fb0f34f762217dba652823e2d7
TerryMiddleton <TerryMiddleton@users.noreply.github.com>
2014-03-02 22:00:28 -0600

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,7 @@ module Xcurrency
   class Application < Rails::Application
     
     # Shopify API connection credentials:
-    config.shopify.api_key = 'ba5ecdd6bc7da798aea7021c01247ff8'
-    config.shopify.secret = '1d30c73776ae89f5abc09a094f7dab8f'
+ 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
